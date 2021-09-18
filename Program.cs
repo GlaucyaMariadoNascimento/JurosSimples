59093f5e793246f8fcd897be3045ac4fd2c2d649
Savioli Pri <stobbeglaucya@gmail.com>
2021-09-18 11:52:59 -0300

    Adicionar resolução exercício JurosSimples

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorCapital,tempoMeses,totalJuros,valorMontante;
+            double valorJuros = 0.02;
+
+            Console.Write("capital [c] (R$).......:");
+            valorCapital = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Tempo [t] (meses)......:");
+            tempoMeses = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Taxa de juros [i] (%)..:");
+            totalJuros = Convert.ToDouble(Console.ReadLine());
+            Console.ReadLine();
+           
+
+            totalJuros = valorCapital*valorJuros*tempoMeses;
+
+            valorMontante = valorCapital+totalJuros;
+
+            Console.WriteLine($"Juros (R$).....: {totalJuros:N}");
+            Console.WriteLine($"Montante (R$)..: {valorMontante:N}");
+
+        }
+    }
+}
